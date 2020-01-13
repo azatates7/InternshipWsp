1d6d3be08a1a3c0f2e3a19563ab11bc63a57e531
Azat Ateş <38978081+azatates7@users.noreply.github.com>
2020-01-13 17:40:00 +0300

    Add files via upload

---
+++ C_Sharp_Mssql_Form_Implements/C_Sharp_Mssql_Form_Implements/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace C_Sharp_Mssql_Form_Implements
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.userbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(424, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 309);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Click += new System.EventHandler(this.selectrow);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(72, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.deleteuser);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(180, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(294, 272);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "INSERT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.insertuser);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(183, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "UPDATE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.updateuser);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(586, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Username";
+            // 
+            // userbox
+            // 
+            this.userbox.Location = new System.Drawing.Point(143, 36);
+            this.userbox.Name = "userbox";
+            this.userbox.Size = new System.Drawing.Size(158, 20);
+            this.userbox.TabIndex = 13;
+            this.userbox.TextChanged += new System.EventHandler(this.searchname);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox userbox;
+    }
+}
+
---
+++ C_Sharp_Mssql_Form_Implements/C_Sharp_Mssql_Form_Implements/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System; 
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing; 
+using System.Windows.Forms;
+
+namespace C_Sharp_Mssql_Form_Implements
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=AA7\\SQLEXPRESS;" + "Trusted_Connection=true;" + "database=testdbb; " + "connection timeout=30";
+
+        public Form1()
+        {
+            InitializeComponent();
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = connstr;
+            cnn.Open();
+            showallrecords();
+            label1.Text = "Connection OK";
+        }
+
+        private void showallrecords()
+        {
+            try
+            {
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = connstr;
+                cnn.Open();
+                String query = "SELECT COUNT(*) FROM userlist";
+                SqlCommand command = new SqlCommand(query, cnn);
+                int count = Convert.ToInt32(command.ExecuteScalar());
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM userlist", cnn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "User");
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    dataGridView1.DefaultCellStyle.BackColor = Color.Azure;
+                    dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.Brown;
+                    dataGridView1.DataSource = ds.Tables["User"];
+                    label2.Text = count + " records founded";
+                }
+                else
+                {
+                    label2.Text = "No records founded";
+                }
+
+                da.Dispose();
+                cnn.Close();
+
+            }
+
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Hata : " + ex);
+            }
+        }
+
+        private void insertuser(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Int32.Parse(textBox1.Text);
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = connstr;
+                cnn.Open();
+                string query = "SELECT * FROM userlist WHERE id=" + id;
+                SqlCommand command = new SqlCommand(query, cnn);
+                SqlDataReader rdr = command.ExecuteReader();
+                if (!rdr.Read())
+                {
+                    rdr.Close();
+                    string name = name = textBox2.Text;
+                    string userkey = textBox3.Text.ToString();
+                    Console.WriteLine(id + name + userkey);
+                    query = "INSERT INTO userlist (id, name, userkey) VALUES (@id, @name, @userkey)";
+                    using (SqlCommand command0 = new SqlCommand(query, cnn))
+                    {
+                        command0.Parameters.AddWithValue("@id", id);
+                        command0.Parameters.AddWithValue("@name", name);
+                        command0.Parameters.AddWithValue("@userkey", userkey);
+
+                        int result = command0.ExecuteNonQuery();
+                    }
+                    cnn.Close();
+                    label1.Text = "Inserted Succcessfully";
+                    textBox1.Text = null; textBox2.Text = null; textBox3.Text = null;
+                    showallrecords();
+                }
+                else
+                {
+                    rdr.Close();
+                    label1.Text = "record founded can't insert";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "I/O Error";
+            }
+        }
+
+        private void updateuser(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = connstr;
+                cnn.Open();
+                string connectionString = connstr;
+                int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+                int key = Convert.ToInt32(selectedRow.Cells["id"].Value);
+                int id = Convert.ToInt32(textBox1.Text);
+                string query = "SELECT * FROM userlist WHERE id=" + id;
+                SqlCommand command = new SqlCommand(query, cnn);
+                SqlDataReader rdr = command.ExecuteReader();
+                if (!rdr.Read())
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        String name = textBox2.Text;
+                        String userkey = textBox3.Text;
+
+                        using (SqlCommand cmd = new SqlCommand("UPDATE userlist SET id=@id, name=@name, userkey=@userkey WHERE id=" + key, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@id", id);
+                            cmd.Parameters.AddWithValue("@name", name);
+                            cmd.Parameters.AddWithValue("@userkey", userkey);
+
+                            int rows = cmd.ExecuteNonQuery();
+                            label1.Text = "Updated Successfully";
+                        }
+                    }
+                    showallrecords();
+                }
+                else
+                {
+                    label1.Text = "Update Error : Double PK";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "I/O Error";
+            }
+        }
+
+        private void selectrow(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+                DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+                textBox1.Text = Convert.ToString(selectedRow.Cells["id"].Value);
+                textBox2.Text = Convert.ToString(selectedRow.Cells["name"].Value);
+                textBox3.Text = Convert.ToString(selectedRow.Cells["userkey"].Value);
+            }
+        }
+
+        private void deleteuser(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Int32.Parse(textBox1.Text);
+                SqlConnection cnn = new SqlConnection();
+                cnn.ConnectionString = connstr;
+                cnn.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM userlist WHERE id=@id", cnn);
+                SqlParameter param = new SqlParameter("@id", id);
+                command.Parameters.Add(param);
+                int result = command.ExecuteNonQuery();
+                label1.Text = "Deleted Succesfully";
+                showallrecords();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "I/O Error";
+            }
+        }
+
+        private void searchname(object sender, EventArgs e)
+        {
+            var dataTable = (DataTable)dataGridView1.DataSource;
+            var dataView = dataTable.DefaultView;
+            dataView.RowFilter = string.Format("Name like '{0}%'", userbox.Text);
+        }
+
+    }
+}
